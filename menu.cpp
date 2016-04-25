c4079bb3ee9a814dd8149178321f16090e384fb5
DevilaN <devilan@o2.pl>
2016-04-25 22:09:44 +0200

    Wstepnie sformatowany plik z opcja czytania z bazy plikowej

--- menu.cpp
+++ menu.cpp
@@ -1,60 +1,80 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
-#include <conio.h>
+#include <cstdio>
+
 using namespace std;
 
-int main()
+
+void wypiszOdzywki(const char* fileName);
+
+
+
+
+int main(int argc, char* argv[])
 {
-	char wybor;
-	while(true)
-	{
-	cout << "MENU GLOWNE" << endl;
-    cout << "-----------------" << endl;
-    cout << "1. Dodanie odzywki" << endl;
-    cout << "2. Usuniecie ostatniej odzywki" << endl;
-    cout << "3. Zapisz odzywke" << endl;
-    cout << "4. Znajdz odzywke" << endl;
-    cout << "5. Wypisanie wszystkich odzywek" << endl;
-    cout << "6. Koniec programu" << endl;
-
-	cout << endl;
- 
-	
-
-
-    wybor=getch();
-    switch(wybor)
-    {
-    case '1':
-            cout << "Dodanie odzywki";
-    break;
-
-    case '2':
-            cout << "Usuniecie ostatniej odzywki";
-    break;
-
-    case '3':
-            cout << "Zapisz odzywke";
-    break;
-
-    case '4':
-    		cout << "Znajdz odzywke";
-    break;
-	
-	case '5':
-			cout << "Wypisanie wszystkich odzywek";
-	break;
-	
-    case '6':
-            exit(0);
-    break;
-
-    default: cout<<"Nie ma takiej opcji w menu!";
-    }
- 
-    getchar();
-    system("cls");
+	if (argc != 2) {
+		cerr << "Nie podano nazwy pliku bazy danych" << endl;
+		return -1;
 	}
+	const char* plikBazy = argv[1];
 
-return 0;
+	while (true) {
+		cout << "MENU GLOWNE" << endl;
+		cout << "-----------------" << endl;
+		cout << "1. Dodanie odzywki" << endl;
+		cout << "2. Usuniecie ostatniej odzywki" << endl;
+		cout << "3. Zapisz odzywke" << endl;
+		cout << "4. Znajdz odzywke" << endl;
+		cout << "5. Wypisanie wszystkich odzywek" << endl;
+		cout << "6. Koniec programu" << endl;
+
+		cout << endl;
+
+		switch (getchar()) {
+			case '1':
+				cout << "Dodanie odzywki" << endl;
+				break;
+
+			case '2':
+				cout << "Usuniecie ostatniej odzywki" << endl;
+				break;
+
+			case '3':
+				cout << "Zapisz odzywke" << endl;
+				break;
+
+			case '4':
+				cout << "Znajdz odzywke" << endl;
+				break;
+
+			case '5':
+				cout << "Wypisanie wszystkich odzywek" << endl;
+				wypiszOdzywki(plikBazy);
+				break;
+
+			case '6':
+				return 0;
+				break;
+
+			default:
+				cout << "Nie ma takiej opcji w menu!" << endl;
+				break;
+		}
+		getchar();	// chwytamy jeszcze znak enter
+	}
+	return 0;
+}
+
+void wypiszOdzywki(const char* fileName)
+{
+	ifstream infile(fileName);
+	string line;
+
+	// Ignore first line
+	getline(infile, line);
+
+	while (getline(infile, line)) {
+		cout << line << endl;
+	}
 }
